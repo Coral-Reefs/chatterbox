e7e0ab147c28c4c71df2e99e0a3c8083ef18ac8f
Joan <nganjoanyou@gmail.com>
2024-08-27 14:03:33 +0800

    update variables

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
   async redirects() {
     return [{ source: "/", destination: "/chats", permanent: true }];
   },
-  env: {
-    NEXT_PUBLIC_ZEGO_APP_ID: "1704147750",
-    NEXT_PUBLIC_ZEGO_SERVER_ID: "f0a8d00c6139416e443406206ee53bad",
-  },
 };
 
 export default nextConfig;
